ae287f7ab4ba39e25813804f7047f72f4e509309
Marshall <leach.marshall@gmail.com>
2025-01-23 13:11:34 -0800

    reduce padding for NewGame

--- app/index.tsx
+++ app/index.tsx
@@ -703,7 +703,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 55,
+        paddingHorizontal: 25,
         },
     skill:{
         flex: 1,
